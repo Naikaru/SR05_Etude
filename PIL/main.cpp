99f1e4c93fe76682cf4dfc2c444982994ad35ecc
paitatho <tom.paita@hotmail.fr>
2019-05-28 18:31:03 +0200

    début construction de la map

--- PIL/main.cpp
+++ PIL/main.cpp
@@ -3,8 +3,18 @@
 int main(int argc, char* argv[]) {
 
     QApplication app(argc, argv);
-    Pil pil(argc, argv);
-    pil.show();
+//    Pil pil(argc, argv);
+//    pil.show();
 
+    Map m;
+    m.show();
+    for(int i = 0; i<20;i++)
+        m.initRobot(i,i,0,0);
+
+    m.move(0,5);
+    m.turn(0,270);
+    m.move(0,5);
+    m.turn(0,90);
+    m.move(0,5);
     return app.exec();
 }
